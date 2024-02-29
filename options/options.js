1acefee2707033cb2d606688290bbf1ff9b6cc3f
Crowdbotics <team@crowdbotics.com>
2024-02-29 22:32:18 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "dbh-silly-little-ap-46733",
+  url: "https://dbh-silly-little-ap-46733.botics.co",
+  api: "https://dbh-silly-little-ap-46733.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
